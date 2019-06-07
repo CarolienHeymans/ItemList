5a971a8097e3779473eba1d0254efe61f54bdd00
Carolien Heymans <heymanscarolien@gmail.com>
2019-06-07 16:35:05 +0200

    first commit

---
+++ Newtonsoft test/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Newtonsoft.Json;
+
+
+namespace Newtonsoft_test
+{
+    public class Product
+    {
+        public string Name;
+       
+        public int Price; 
+        public Product(string name, int price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        }
+     
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Product product1 = new Product("a", 1);
+
+            product1.Name = "Apple";
+          
+            product1.Price = 2;
+
+            Product product2 = new Product("a", 1);
+            product2.Name = "Pear";
+
+            product2.Price = 3;
+
+
+
+            string output = JsonConvert.SerializeObject(product2);
+            Console.WriteLine(output);
+        }
+    }
+}
